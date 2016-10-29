4b47e28b6e9d5c71e6ac41e3ff4fcc0a6207234a
Gabriel.Sturtevant <Gabriel@GabrielSturtevant.com>
2016-10-29 01:15:15 -0700

    Added documentation

--- fib/fib.php
+++ fib/fib.php
@@ -5,18 +5,23 @@
  * Date: 10/28/16
  * Time: 6:32 PM
  */
-    function fib($tot)
+
+/**
+ * Number of iterations of the Fibonacci sequence to compute
+ * @param int $tot
+ */
+ function fib($tot)
+ {
+    $a = 0;
+    $b = 1;
+    for($i = 0; $i <= $tot; $i++)
     {
-        $a = 0;
-        $b = 1;
-        for($i = 0; $i <= $tot; $i++)
-        {
-            $temp = $b;
-            $b = $a;
-            $a = $a + $temp;
-            echo $a . "<br>";
-        }
+        $temp = $b;
+        $b = $a;
+        $a = $a + $temp;
+        echo $a . "<br>";
     }
-    $itterations = $_SERVER['QUERY_STRING'];
-    fib($itterations);
+ }
+ $iterations = $_SERVER['QUERY_STRING'];
+ fib($iterations);
 ?>
